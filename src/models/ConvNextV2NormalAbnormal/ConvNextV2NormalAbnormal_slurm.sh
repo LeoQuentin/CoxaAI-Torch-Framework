b21d0b04abd9afbea8003d34eaa28af666db36a1
LeoQuentin <leo.quentin.thorbjornsen.bakholt@nmbu.no>
2024-02-25 09:26:58 +0100

    found error, gotta use basic slurm script with no variables

--- src/models/ConvNextV2NormalAbnormal/ConvNextV2NormalAbnormal_slurm.sh
+++ src/models/ConvNextV2NormalAbnormal/ConvNextV2NormalAbnormal_slurm.sh
@@ -1,22 +1,19 @@
 #!/bin/bash
-
-# Define the Python script filename
-filename="ConvNextV2NormalAbnormal.py"
-
-#SBATCH --ntasks-per-node=16 # 16 cores (CPU)
+#SBATCH --ntasks-per-node=16          # 16 cores (CPU)
 #SBATCH --nodes=1            # Use 1 node
 #SBATCH --job-name=training  # Name of job
 #SBATCH --partition=gpu      # Use GPU partition
 #SBATCH --gres=gpu:1         # Use one GPUs
 #SBATCH --mem=64G            # Default memory per CPU is 3GB
-#SBATCH --output=/mnt/users/leobakh/VET_project/VET-Special-syllabus/logs/output_logs/${filename}_%j.out  # Dynamic output filename based on Python script name
+#SBATCH --output=./output_logs/training_prints_%j.out # Stdout file
+
 ## Script commands
 module load singularity
 
 SIFFILE="/mnt/users/leobakh/VET_project/VET-Special-syllabus/singularity/container_pytorch_cv.sif" ## FILL INN
-
 ## RUN THE PYTHON SCRIPT
-singularity exec --nv $SIFFILE python $filename
+# Using a singularity container named container_u_net.sif
+singularity exec --nv $SIFFILE python ConvNextV2NormalAbnormal.py        ## FILL INN
 
-# Send this job into the slurm queue with the following command: 
-# >> sbatch test_script_slurm.sh 
+# Send this job into the slurm queue with the following command:
+# >> sbatch test_script_slurm.sh
