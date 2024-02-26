edc3af006b8ff46b507552bd3595390f6efe7bc9
LeoQuentin <leo.quentin.thorbjornsen.bakholt@nmbu.no>
2024-02-26 22:27:41 +0100

    trying to fix transfer learning model

--- src/models/EfficientNetB7TransferNormalAbnormal/EfficientNetB7TransferNormalAbnormal.py
+++ src/models/EfficientNetB7TransferNormalAbnormal/EfficientNetB7TransferNormalAbnormal.py
@@ -19,31 +19,6 @@ if project_root:
 from src.models.BaseNormalAbnormal import BaseNormalAbnormal # noqa
 from src.utilities.H5DataModule import H5DataModule # noqa
 
-
-preprocessor = EfficientNetImageProcessor.from_pretrained("google/efficientnet-b7")
-
-
-def preprocess_image(image: torch.Tensor):
-    data = preprocessor(images=image, return_tensors="pt")
-    print(data["pixel_values"].shape)
-    return data.input
-
-
-
-dm = H5DataModule(os.getenv("DATA_FILE"),
-                  batch_size=12,
-                  train_folds=[0, 1, 2],
-                  val_folds=[3],
-                  test_folds=[4],
-                  target_var='target',
-                  tf_to_torch_channelswap=True,
-                  stack_channels=True,
-                  train_transform=preprocess_image,
-                  val_transform=preprocess_image,
-                  test_transform=preprocess_image
-                  )
-
-
 # --------------------- Model ---------------------
 
 class EfficientNetB7TransferNormalAbnormal(BaseNormalAbnormal):
@@ -66,7 +41,7 @@ def preprocess_image(image: torch.Tensor):
 # --------------------- DataModule ---------------------
 
 dm = H5DataModule(os.getenv("DATA_FILE"),
-                  batch_size=2,
+                  batch_size=12,
                   train_folds=[0, 1, 2],
                   val_folds=[3],
                   test_folds=[4],
