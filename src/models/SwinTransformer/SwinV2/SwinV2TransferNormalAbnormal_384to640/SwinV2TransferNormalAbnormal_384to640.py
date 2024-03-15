0618c904a050323cc39534425a74b713fcc4effe
LeoQuentin <leo.quentin.thorbjornsen.bakholt@nmbu.no>
2024-03-15 19:47:55 +0100

    reduced batch size for training on 640*640

--- src/models/SwinTransformer/SwinV2/SwinV2TransferNormalAbnormal_384to640/SwinV2TransferNormalAbnormal_384to640.py
+++ src/models/SwinTransformer/SwinV2/SwinV2TransferNormalAbnormal_384to640/SwinV2TransferNormalAbnormal_384to640.py
@@ -38,7 +38,7 @@ if __name__ == "__main__":
 
     # Training parameters
     training_params = {
-        "batch_size": 16,
+        "batch_size": 12,
         "early_stopping_patience": 40,
         "max_time_hours": 12,
         "train_folds": [0, 1, 2, 3],
