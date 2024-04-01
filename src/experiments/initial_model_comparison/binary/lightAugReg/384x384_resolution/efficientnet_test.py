8e170607c186a0174ac6a6c22d418d3580f2dde2
LeoQuentin <leo.quentin.thorbjornsen.bakholt@nmbu.no>
2024-04-01 15:57:29 +0200

    added some funcs

--- src/experiments/initial_model_comparison/binary/lightAugReg/384x384_resolution/efficientnet_test.py
+++ src/experiments/initial_model_comparison/binary/lightAugReg/384x384_resolution/efficientnet_test.py
@@ -1,5 +1,63 @@
-import torch
+import torch # noqa
+from torchvision import transforms
+import os
+from datetime import timedelta
+import sys
+import dotenv
+
+# huggingface model
 from transformers import AutoConfig, AutoModelForImageClassification
+# Lightning
+from pytorch_lightning import Trainer
+from pytorch_lightning.callbacks import EarlyStopping, ModelCheckpoint, LearningRateMonitor
+from pytorch_lightning.loggers import CSVLogger
+from torch.optim.lr_scheduler import ReduceLROnPlateau
+
+dotenv.load_dotenv()
+project_root = os.getenv('PROJECT_ROOT')
+if project_root:
+    sys.path.append(project_root)
+from src.models.BaseNormalAbnormal import BaseNormalAbnormal # noqa
+from src.utilities.H5DataModule import H5DataModule # noqa
+from src.utilities.np_image_to_PIL import np_image_to_PIL # noqa
+from src.augmentation.autoaugment import ImageNetPolicy # noqa
+
+size = (384, 384)
+
+def train_preprocess(image):
+    # image is a numpy array in the shape (H, W, C)
+    image = np_image_to_PIL(image)  # convert to PIL image
+
+    # Preprocess the image
+    transform_pipeline = transforms.Compose([
+        transforms.Resize(size),
+        transforms.RandomRotation(10),
+        transforms.Grayscale(num_output_channels=1),
+        transforms.RandomHorizontalFlip(),
+        transforms.ToTensor()
+    ])
+    image = transform_pipeline(image)
+
+    # Remove the batch dimension if it exists
+    if len(image.size()) == 4:
+        image = image.squeeze(0)
+    return image
+
+
+def val_test_preprocess(image):
+    # basically same as train_preprocess but without the augmentations
+    image = np_image_to_PIL(image)  # convert to PIL image
+
+    transform_pipeline = transforms.Compose([
+        transforms.Resize(size),
+        transforms.Grayscale(num_output_channels=1),
+        transforms.ToTensor()
+    ])
+    image = transform_pipeline(image)
+
+    if len(image.size()) == 4:
+        image = image.squeeze(0)
+    return image
 
 
 class EfficientNet_384(torch.nn.Module):
@@ -23,4 +81,3 @@ config.num_channels = 1
 
 model = EfficientNet_384(config)
 model.load_state_dict(state_dict)
-
