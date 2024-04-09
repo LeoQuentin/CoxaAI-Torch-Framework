9578ef325bed6e473678e7eed0fe7e2cabec8126
LeoQuentin <leo.quentin.thorbjornsen.bakholt@nmbu.no>
2024-04-09 10:20:33 +0200

    pls work

--- coxaaitorch/experiments/SwinBinaryVsMulticlass/SwinBinaryVsMulticlass.sh
+++ coxaaitorch/experiments/SwinBinaryVsMulticlass/SwinBinaryVsMulticlass.sh
@@ -3,7 +3,7 @@
 #SBATCH --nodes=1            # Use 1 node
 #SBATCH --job-name=CoxaAI-ViT  # Name of job
 #SBATCH --partition=gpu      # Use GPU partition
-#SBATCH --gres=gpu:1         # Use one GPUs
+#SBATCH --gpus-per-node=2  # Request 2 GPUs per node 
 #SBATCH --mem=64G            # Default memory per CPU is 3GB
 #SBATCH --output=./output_logs/training_prints_%j.out # Stdout file
 
@@ -13,6 +13,6 @@ module load singularity
 SIFFILE="/mnt/users/leobakh/VET_project/VET-Special-syllabus/singularity/container_CoxaAI_Poetry.sif" ## FILL INN
 ## RUN THE PYTHON SCRIPT
 # Using a singularity container named container_u_net.sif
-singularity exec --nv $SIFFILE srun python SwinBinaryVsMulticlass.py
+srun singularity exec --nv $SIFFILE python SwinBinaryVsMulticlass.py
 # Send this job into the slurm queue with the following command:
 # >> sbatch test_script_slurm.sh
